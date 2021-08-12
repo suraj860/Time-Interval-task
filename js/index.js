02038752979ab4c9a5de6b79e1ef7a55385d9caa
suraj860 <surajpatil131297@gmail.com>
2021-08-11 22:50:35 -0700

    slide show (time interval)

---
+++ js/index.js
@@ -0,0 +1,43 @@
+
+var index = 0;
+const img = document.querySelectorAll("img");
+
+//logic for slide show and next button//
+var slideShow = ()=>{
+   
+    for(i=0 ; i<img.length ; i++){
+       img[i].style.display="none";
+    }
+
+    index++
+    if(index>img.length){
+        index=1;
+    }
+    img[index -1].style.display = "block";   
+}
+
+
+//prev button logic//
+
+var pre = ()=>{
+    for(i=0 ; i<img.length ; i++){
+        img[i].style.display="none";
+     }
+     if(index===0){
+         index=5
+     }
+     img[index -1].style.display = "block"; 
+     index--    
+}
+
+let timeing;
+
+function start (){
+     timeing = setInterval(slideShow, 2000);
+}
+start();
+
+function stop(){
+    clearInterval(timeing);
+}
+
